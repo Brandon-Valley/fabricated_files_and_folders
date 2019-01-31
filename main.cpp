ba348c4cbdbc72d3b40dede8a4d62de3169ad6c6
Brandon Valley <bavvh8@mst.edu>
2019-01-31 14:45:05 -0600

    made crappy ls

--- main.cpp
+++ main.cpp
@@ -12,8 +12,16 @@ int main()
 {
 	cout << "hi there soifnhiosh" << endl;
 
-	Dir root = Dir();
+	Dir root = Dir("");
+	Dir *cur_dir = &root;
+
 	cout << root.m_owning_user << endl;
-	//
+	cout << cur_dir->m_owning_user << endl;
+	cout << cur_dir->m_name << endl;
+	cur_dir->mkdir("jeff");
+	cur_dir->mkdir("bob");
+
+	cout << cur_dir->m_child_dir_vec.size() << endl;
+	cur_dir->ls();
 
 }
