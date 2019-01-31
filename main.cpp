1505233c256accefa5a97d57b8853d1e99388cce
Brandon Valley <bavvh8@mst.edu>
2019-01-31 16:46:02 -0600

    pwd working

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main()
 	cout << "hi there soifnhiosh" << endl;
 
 	//root is the top level directory
-	Dir root = Dir("root");
+	Dir root = Dir(ROOT_M_NAME);
 	Dir *cur_dir = &root;
 
 	cout << root.m_owning_user << endl;
@@ -29,9 +29,12 @@ int main()
 	cur_dir->mkdir("joe");
 	cur_dir->ls();
 
-	cout << cur_dir->m_parent_dir_p->m_name << endl;
+	cout << "here" << endl;
+	cur_dir->pwd();
 
-	cur_dir = cur_dir->cd("..");
-	cur_dir->ls();
+//	cout << cur_dir->m_parent_dir_p->m_name << endl;
+//
+//	cur_dir = cur_dir->cd("..");
+//	cur_dir->ls();
 
 }
